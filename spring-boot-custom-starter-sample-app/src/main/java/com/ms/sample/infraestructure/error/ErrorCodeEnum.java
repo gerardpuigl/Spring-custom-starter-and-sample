42bee048fd0c26677c2ebae314102ab3ff74898b
Gerard Puig <gerard.puig@wefox.com>
2024-06-26 13:26:57 +0200

    sample failed

--- spring-boot-custom-starter-sample-app/src/main/java/com/ms/sample/infraestructure/error/ErrorCodeEnum.java
+++ spring-boot-custom-starter-sample-app/src/main/java/com/ms/sample/infraestructure/error/ErrorCodeEnum.java
@@ -9,7 +9,8 @@ import lombok.RequiredArgsConstructor;
 public enum ErrorCodeEnum {
   SAMPLE_NOT_FOUND(new ErrorCode("Sample not found", "SP_01", 404)),
   UNEXPECTED_SAMPLE_NOT_FOUND(new ErrorCode("Unexpected sample not found", "SP_02", 500)),
-  SAMPLE_PROCESS_ALREADY_INITIATED(new ErrorCode("The sample is already being processed", "SP_03", 409));
+  SAMPLE_PROCESS_ALREADY_INITIATED(new ErrorCode("The sample is already being processed", "SP_03", 409)),
+  SAMPLE_PROCESS_FAILED(new ErrorCode("The sample process failed", "SP_04", 500));
 
   private final ErrorCode errorCode;
 
