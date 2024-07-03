2c2fd12553a34db975202eff0498eec347e0f024
Gerard Puig <gerard.puig@wefox.com>
2024-07-03 16:06:51 +0200

    Include missing @Transactional annotation

--- spring-boot-custom-starter-sample-app/src/main/java/com/ms/sample/application/ProcessSampleUseCase.java
+++ spring-boot-custom-starter-sample-app/src/main/java/com/ms/sample/application/ProcessSampleUseCase.java
@@ -14,6 +14,7 @@ import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
@@ -24,8 +25,8 @@ public class ProcessSampleUseCase implements ProcessSampleInPort {
   private final SampleEventOutPort sampleEventOutPort;
 
   @Override
+  @Transactional
   public void execute(UUID sampleId) {
-
     Sample sample = getSample(sampleId);
     processSample(sample);
   }
